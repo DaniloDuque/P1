a699c5d7b8c40dc46a1f898c2e2a105b7dcdc9ad
DaniloDuque <dduque@estudiantec.cr>
2025-01-12 15:22:13 -0600

    changes on cup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,9 +26,6 @@ public class Main {
             Parser parser = new Parser(lexer);
             parser.parse();
 
-
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
